0f67f5a91fe8353e580853f6cef7c161696f8b67
Lou Tigroudja <lou.tigroudja07@gmail.com>
2024-10-25 09:39:23 +0200

    Retrait de la lib util inutile

--- back/server.js
+++ back/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const app = express();
 const port = 3000;
